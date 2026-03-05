b9afafaf581b6181ae1fd4eed09376b34c41934c
Linus Wabwire <gymnottwc@gmail.com>
2026-03-05 15:37:13 +0300

    csss

--- api.py
+++ api.py
@@ -21,42 +21,63 @@ if not os.path.exists(SCAN_DIR):
     os.makedirs(SCAN_DIR)
 
 def run_semgrep_scan(code_content, language):
-   
-    unique_id = str(uuid.uuid4())
-    ext_map = {"python": ".py", "javascript": ".js", "java": ".java", "c++": ".cpp"}
-    extension = ext_map.get(language.lower(), ".txt")
     
+    lang = language.lower().strip()
+    ext_map = {
+        "python": ".py", 
+        "javascript": ".js", 
+        "js": ".js",
+        "typescript": ".ts", 
+        "java": ".java", 
+        "c++": ".cpp",
+        "cpp": ".cpp",
+        "c": ".c",
+        "go": ".go",
+        "php": ".php",
+        "sql": ".sql"
+    }
+    
+    extension = ext_map.get(lang, ".txt")
+    
+    unique_id = str(uuid.uuid4())
     temp_path = os.path.join(SCAN_DIR, f"scan_{unique_id}{extension}")
 
     with open(temp_path, "w") as f:
         f.write(code_content)
-    
-    print(f" Created unique file for scanning: {temp_path}")
 
     try:
-       
-        cmd = ["semgrep", "--config=auto", "--json", temp_path]
+        
+        cmd = ["semgrep", "--config=p/security-audit", "--config=p/secrets",  "--config=p/default", "--json", temp_path ]
+                
         result = subprocess.run(cmd, capture_output=True, text=True)
         
-        if not result.stdout: return []
+        if result.stderr:
+            print(f"⚠️ Semgrep CLI Error: {result.stderr}")
+
+        if not result.stdout:
+            return []
+
         raw_data = json.loads(result.stdout)
-        
         findings = []
+
         for issue in raw_data.get("results", []):
             findings.append({
                 "title": issue["check_id"].split(".")[-1].title(),
                 "severity": issue["extra"]["severity"],
                 "line": issue["start"]["line"],
                 "description": issue["extra"]["message"],
+                "remediation": "Follow OWASP best practices to sanitize this input.",
                 "tool": "Semgrep"
             })
+        
         return findings
 
+    except Exception as e:
+        print(f"❌ Backend Scan Error: {e}")
+        return []
     finally:
-        
         if os.path.exists(temp_path):
             os.remove(temp_path)
-            print(f"🗑️ Deleted {temp_path} after scan.")
 
 @app.post("/api/scan")
 def scan_snippet(request: dict):
